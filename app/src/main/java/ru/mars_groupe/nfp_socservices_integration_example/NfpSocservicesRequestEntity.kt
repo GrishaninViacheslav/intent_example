a98e00207fc81333024aa2ae8b5a404d19a2cc9d
Slava <v.grishanin@megar.ru>
2024-01-10 12:39:58 +0300

    Описание полей

--- app/src/main/java/ru/mars_groupe/nfp_socservices_integration_example/NfpSocservicesRequestEntity.kt
+++ app/src/main/java/ru/mars_groupe/nfp_socservices_integration_example/NfpSocservicesRequestEntity.kt
@@ -1,10 +1,10 @@
 package ru.mars_groupe.nfp_socservices_integration_example
 
 data class NfpSocservicesRequestEntity(
-    val orgCode: String?,
-    val employeeCode: String,
+    val orgCode: String?, // nfpcore.org_legal_person_employees.owner_code
+    val employeeCode: String, // nfpcore.org_legal_persons.owner_code
     val control_services: Boolean,
-    val certificate_id: String?,
-    val person_id: String?,
+    val certificate_id: String?, // nfpcore.crt_certificates.cert_number
+    val person_id: String?, // nfpcore.psn_person_identifiers.owner_soc_id
     val services: List<NfpSocservicesServiceIdEntity>
 )
--- app/src/main/java/ru/mars_groupe/nfp_socservices_integration_example/NfpSocservicesServiceIdEntity.kt
+++ app/src/main/java/ru/mars_groupe/nfp_socservices_integration_example/NfpSocservicesServiceIdEntity.kt
@@ -1,5 +1,5 @@
 package ru.mars_groupe.nfp_socservices_integration_example
 
 data class NfpSocservicesServiceIdEntity(
-    val service_id: String
+    val service_id: String // nfpcore.soc_services.code
 )
